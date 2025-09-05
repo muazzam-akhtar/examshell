f461a91faa66968d22d4b4dffcb65f4a675069b5
Muazzam Akhtar <muazzamakhtar@Muazzams-MacBook-Pro.local>
2025-09-05 20:54:09 +0400

    ft_sort_params #2

--- ft_sort_params/ft_sort_params.c
+++ ft_sort_params/ft_sort_params.c
@@ -25,26 +25,12 @@ int ft_strcmp(const char *s1, const char *s2)
 {
     int i = 0;
 
-    if (ft_strlen(s1) > ft_strlen(s2))
-    {
-        while (i < ft_strlen(s2))
-        {
-            if (s1[i] - s2[i] != 0)
-                return (s1[i] - s2[i]);
-            i++;
-        }
-    }
-    else
-    {
-        while (i < ft_strlen(s1))
-        {
-            if (s1[i] - s2[i] != 0)
-                return (s1[i] - s2[i]);
-            i++;
-        }
-    }
-    return (0);
+    while (s1[i] && s2[i] && s1[i] == s2[i])
+        i++;
+
+    return (s1[i] - s2[i]);
 }
+
 
 void    ft_swap(char **a, char **b)
 {
@@ -74,7 +60,7 @@ int main(int argc, char **argv)
 {
     if (argc == 1)
         return (0);
-    if (argc > 2)
+    if (argc > 1)
     {
         char    **ret = sort_args(argc, argv);
         int i = 1;
@@ -84,7 +70,5 @@ int main(int argc, char **argv)
             i++;
         }
     }
-    else
-        ft_putendl(1, argv[1]);
     return (0);
 }
